17a6ae4e0e7855773aee48372798c36a8a24eacf
Foncham <foncham@Fonchams-MacBook-Pro.local>
2024-08-31 07:09:25 +0100

    first commit

---
+++ src/main/java/com/teodigital/springmodulithcourse/SpringModulithCourseApplication.java
@@ -0,0 +1,13 @@
+package com.teodigital.springmodulithcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringModulithCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringModulithCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teodigital/springmodulithcourse/SpringModulithCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teodigital.springmodulithcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModulithCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
